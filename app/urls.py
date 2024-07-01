2e572c3e8fb16248742e855a30d0fbbfb82d1754
Mael Fosso <tchindefossomael@yahoo.fr>
2024-07-01 21:04:46 +0200

    update

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,7 @@
+from app import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-
-from app import views
 
 urlpatterns=[
   path('', views.home, name='home'),
@@ -10,3 +9,4 @@ urlpatterns=[
   path('logout',views.logout,name='logout'),
   path('register',views.register,name='register'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
